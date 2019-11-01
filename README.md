d50a5e67c7fb7a558d1646f0e1eb7d1d7e96c75c
Cian McGinn <cian.mcginn@gmail.com>
2019-10-31 23:51:01 -0600

    Initial commit (aka the whole project)

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # combo-marker-slp
 Helps keep track of ssbm combos with Project Slippi
+
+Renames files to timestamps of user saved moments
+
+# In game
+Hit D-Pad down twice while in game to mark combo or other cool moment
+
+# Running the script
+Recursively checks all files and directories from starting point, renaming all files where a player pressed d-pad down to a list of times
+Can run with no arguments to search the directory the script is in.
+Can run with a path to start the search at as command line argument.
+#Warning! running will rename any marked replay, so if you have your files organized with good names you may want to edit this script
+
+
+# Dependencies
+Requires py-slippi - https://pypi.org/project/py-slippi/
+
+
---
+++ combo-marker-slp.py
@@ -0,0 +1,51 @@
+from slippi import Game
+from slippi import event
+import os
+import sys
+
+def main():
+    if(len(sys.argv) > 1):
+        rootDir = sys.argv[1]
+    else:
+        rootDir = "."
+    print(rootDir)
+    for root, dirs, files in os.walk(rootDir):
+        for f in files:
+            if(f[len(f) - 4:] == ".slp"):
+                mark(root, f)
+
+def mark(root, f):
+    game = Game(root + "/" + f)
+    frames = game.frames
+    found = [False, False, False, False]
+    endSearch = [0, 0, 0, 0]
+    markedFrames = []
+    finishedFrames = []
+    for i in range(len(frames)):
+        frame = frames[i]
+        ports = frame.ports
+        for j in range(len(ports)):
+            if(endSearch[j] < i):
+                found[j] = False
+            if(not ports[j]):
+                continue
+            if(ports[j].leader.pre.buttons.physical == event.Buttons.Physical.DPAD_DOWN):
+                if(found[j]):
+                    continue
+                if(i < endSearch[j]):
+                    markedFrames.append(i)
+                    found[j] = True
+                    continue;
+                endSearch[j] = i + 120
+    for frame in markedFrames:
+        minute = 7 - (int(frame / 60 / 60))
+        second = 62 - (int(frame / 60 % 60))
+        frame = str(minute) + "-" + str(second)
+        finishedFrames.append(frame)
+    name = ""
+    for frame in finishedFrames:
+        name = name + str(frame) + ", "
+    os.rename(r'' + root + "/" + f, r'' + root + "/" + name[:len(name) - 2] + '.slp')
+
+        
+main()
